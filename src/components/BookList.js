b4a3a311e1185a8f1a281c53b74b28481896afb6
Samir Valiyev <samir.valiyev@gmail.com>
2021-03-25 01:32:25 +0400

    context api first lesson

---
+++ src/components/BookList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Book from '../components/Book';
+
+class BookList extends React.Component {
+
+    render() {
+
+        const bookList = this.props.books.map((book, i) => {
+            return <Book book={book} key={i} />;
+        });
+
+        return (
+            <div>
+                {bookList}
+            </div>
+        );
+    }
+}
+export default BookList;
