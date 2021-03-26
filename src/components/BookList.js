afb884d3ba387556fa98311416aa87b10800ab37
Samir Valiyev <samir.valiyev@gmail.com>
2021-03-27 00:40:02 +0400

    added boostrap and fontawesome

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import Book from '../components/Book';
+
+import Book from './Book';
+import './BookLikst.scss';
 
 class BookList extends React.Component {
 
     render() {
 
+        // console.log(this.props.books)
+
         const bookList = this.props.books.map((book, i) => {
-            return <Book book={book} key={i} />;
+            return <Book book={book}
+                key={i}
+            />;
         });
 
         return (
-            <div>
-                {bookList}
-            </div>
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">BookFolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
+
+                        {bookList}
+
+                    </div>
+                </div>
+            </section>
         );
     }
+
 }
+
 export default BookList;
+
+
